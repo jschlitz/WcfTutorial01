aff3fd87877697d1fce9a0d8014db2bfb99755de
Jacob Schlitz <jschlitz@gmail.com>
2014-07-13 15:25:43 -0400

    The instructions are starting to go off kilter.

---
+++ CalculatorHost/Program.cs
@@ -0,0 +1,57 @@
+﻿using GettingStartedLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorHost
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      //make uri
+      var baseAddress = new Uri("http://localhost:8000/GettingStarted/");
+
+      //step 2
+      var selfHost = new ServiceHost(typeof(CalculatorService), baseAddress);
+
+      try
+      {
+        //endpoint
+        selfHost.AddServiceEndpoint(typeof(ICalculator), new WSHttpBinding(), "CalculatorService");
+
+        //metadata
+        var smb = new ServiceMetadataBehavior();
+        smb.HttpGetEnabled = true;
+        smb.MetadataExporter.PolicyVersion = PolicyVersion.Policy15;
+        selfHost.Description.Behaviors.Add(smb);
+        
+        //var foo =selfHost.AddDefaultEndpoints();
+
+        //foreach (var item in foo)
+        //{
+        //  Console.WriteLine("----------");
+        //  Console.WriteLine(item.Address.ToString());
+        //}
+
+        //begin
+        selfHost.Open();
+        Console.WriteLine("Calculator service started. Press enter to terminate.");
+        Console.WriteLine();
+        Console.ReadLine();
+
+        selfHost.Close();
+      }
+      catch (CommunicationException ex)
+      {
+        Console.WriteLine("Exception! {0}", ex.Message);
+        selfHost.Abort();
+        Console.ReadLine();
+      }
+    }
+  }
+}
--- GettingStartedLib/CalculatorService.cs
+++ GettingStartedLib/CalculatorService.cs
@@ -8,7 +8,7 @@ using System.Text;
 namespace GettingStartedLib
 {
   // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
-  public class Calculator : ICalculator
+  public class CalculatorService : ICalculator
   {
     public double Add(double n1, double n2)
     {
